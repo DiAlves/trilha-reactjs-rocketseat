3d3239592020114a043e43a0684d3ac4d58f8bff
Diego Alves <diegoalves@digix.com.br>
2025-03-17 19:07:04 -0300

    Configuration of the Task List Header component.

--- desafio-01/src/components/list/Header.tsx
+++ desafio-01/src/components/list/Header.tsx
@@ -9,7 +9,7 @@ export function Header({ tasks }: HeaderProps ) {
   return (
     <div className={styles.header}>
       <strong>Tarefas criadas <span className={styles.count}> {tasks.length} </span></strong>
-      <strong>Conluídas <span className={styles.count}> {tasks.filter(tasks => tasks.isChecked).length} </span></strong>
+      <strong>Concluídas <span className={styles.count}> {tasks.filter(tasks => tasks.isChecked).length} </span></strong>
     </div>
   );
 }
